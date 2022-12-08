d2901cfe4647216c749dddee80a8395d51b5346d
Emanueluz <60400509+Emanueluz@users.noreply.github.com>
2022-12-08 20:18:47 -0300

    Delete image directory

--- image/controller.js
+++
@@ -1,9 +0,0 @@
-const { StatusCodes } = require('http-status-codes')
-
-const img = async (req, res) => {
-    return res.status(StatusCodes.OK).json({"link": "https://picsum.photos/200/300"})
-}
-
-module.exports = {
-    img,
-}
--- image/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const { img } = require('./controller')
-
-router.get('/', img)
-
-module.exports = router
